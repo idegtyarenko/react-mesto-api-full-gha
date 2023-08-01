85a0759f3187bcbce7c3b5ac522b1eed8112bdb8
Ivan Degtiarenko <degat@yandex.ru>
2023-08-01 21:41:54 +0100

    fix: No connection to mongo on server

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,5 +1,5 @@
 export const JWT_KEY = 'some-secret-key';
-export const DATABASE_URL = 'mongodb://localhost:27017/mestodb';
+export const DATABASE_URL = 'mongodb://127.0.0.1:27017/mestodb';
 
 export const messages = {
   NOT_FOUND: 'Ресурс не найден',
