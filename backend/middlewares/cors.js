d264d63b82605ed4e14c43bcf30af85aaa9b41a1
Ivan Degtiarenko <degat@yandex.ru>
2023-08-02 10:25:08 +0100

    fix: Preflight requests fail

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,13 +9,14 @@ export default function cors(req, res, next) {
 
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-  }
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.end();
   }
 
   next();
