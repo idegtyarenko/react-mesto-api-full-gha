210a788c2e195190e0d0b00f690bb308bab28c3f
Ivan Degtiarenko <degat@yandex.ru>
2023-08-03 23:51:33 +0100

    fix: Too low limiter value

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const app = express();
 mongoose.connect(DATABASE_URL);
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true,
   legacyHeaders: false,
 });
